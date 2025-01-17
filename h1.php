6b32f2e9bc1678ff5012df26afea966f6e113026
JirapatSiri <24490@srithatpit.ac.th>
2025-01-17 11:28:57 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+     <p>การแสดงข้อความหัวข้อ</p>
+     <div>
+        <h1>HTMLเบื้องต้น(h1)</h1>
+     </div>
+     <div>
+        <h2>HTMLเบื้องต้น(h2)</h2>
+        <h3>HTMLเบื้องต้น(h2)</h3>
+        <h4>HTMLเบื้องต้น(h2)</h4>
+        <h5>HTMLเบื้องต้น(h2)</h5>
+        <h6>HTMLเบื้องต้น(h2)</h6>
+     </div>
+     <p>-----การสร้างข้อความตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ-----</p>
+
+     <div>
+         <b>จิรภัทร ศิริ</b><br>
+         <i>จิรภัทร ศิริ</i><br>
+         <u>จิรภัทร ศิริ</u><br>
+     </div>  
+     <p><b>-----การ Link-----</p></b>
+     <div>
+         <!--Linkหน้าเดียว-->
+         <a href = "https://www.google.co.th">Google</a><br>
+     <div>    
+     
+</body>
+</html>
